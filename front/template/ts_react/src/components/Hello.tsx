481dcc2eee0aa6c78bbef64a814a77b05a42666b
zsuzitor <Avdosevstas@mail.ru>
2020-12-06 15:02:51 +0400

    +

--- front/template/ts_react/src/components/Hello.tsx
+++ front/template/ts_react/src/components/Hello.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 
-export interface HelloProps {
+export interface IHelloProps {
     compiler: string;
     framework: string;
     // children: JSX.Element;//указав так, можно рендерить компонент вкладывая в него другой <HelloComponent name="foo"><h1>Hello World</h1></HelloComponent>
 }
 
-export class Hello extends React.Component<HelloProps, {}> {
+export class Hello extends React.Component<IHelloProps, {}> {// <props, state>
     render() {
         return <h1>Привет от {this.props.compiler} и {this.props.framework}!</h1>;
     }
